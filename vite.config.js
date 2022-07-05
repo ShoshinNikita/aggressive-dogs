959aa8b385bfa2033ded158fafbe386ff7b73a88
Nikita Shoshin <shoshin_nikita@fastmail.com>
2022-07-06 00:50:53 +0400

    update `vite` config to generate relative links

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	// Generate relative links: https://github.com/vitejs/vite/issues/762#issuecomment-711167905
+	base: ""
+});
